8c5486aa99243095e596874e27abf6e8f9f828f7
Boomtime12 <ryandemolina@gmail.com>
2025-04-17 12:41:41 -0400

    Created class structure and implemented insertEdge, insertCoordinates, getAdjacent, and getDistance

--- src/EmergencyResponseGraph.cpp
+++ src/EmergencyResponseGraph.cpp
@@ -1,2 +1,34 @@
-
 #include "src/EmergencyResponseGraph.h"
+
+
+void EmergencyResponseGraph::insertEdge(int source, int dest, float dist) {
+    adjList[source].push_back(make_pair(dest, dist));
+    if (adjList.find(dest) == adjList.end()) {
+        adjList[dest] = {};
+    }
+}
+
+void EmergencyResponseGraph::insertCoordinates(int vertex, float lat, float lon) {
+    if (coordinates.find(vertex) == coordinates.end()) {
+        coordinates[vertex] = make_pair(lat, lon);
+    }
+}
+
+vector<int> EmergencyResponseGraph::getAdjacent(int vertex) {
+    vector<int> adjacent;
+    for (pair<int, float> adj: adjList[vertex]) {
+        adjacent.push_back(adj.first);
+    }
+    return adjacent;
+}
+
+float EmergencyResponseGraph::getDistance(int source, int dest) {
+    for (pair<int, float> adj: adjList[source]) {
+        if (adj.first == dest) {
+            return adj.second;
+        }
+    }
+    return 0;
+}
+
+
--- src/EmergencyResponseGraph.h
+++ src/EmergencyResponseGraph.h
@@ -1,6 +1,18 @@
+#include <vector>
+#include <unordered_map>
 
+using namespace std;
 
 class EmergencyResponseGraph {
+    private:
+        unordered_map<int, vector<pair<int, float>>> adjList;
+        unordered_map<int, pair<float, float>> coordinates;
 
+    public:
+        EmergencyResponseGraph() {};
+        void insertEdge(int source, int dest, float dist);
+        void insertCoordinates(int vertex, float lat, float lon);
+        vector<int> getAdjacent(int vertex);
+        float getDistance(int source, int dest);
 };
 
