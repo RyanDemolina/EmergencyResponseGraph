885d1304c7add1951246c01a192f2f6c23dc0f11
Boomtime12 <ryandemolina@gmail.com>
2025-04-22 19:15:39 -0400

    Add comments to files

--- src/EmergencyResponseGraph.cpp
+++ src/EmergencyResponseGraph.cpp
@@ -1,22 +1,26 @@
 #include "src/EmergencyResponseGraph.h"
 #include <tuple>
 
-
+// Creates a vertex with no adjacent vertices
 void EmergencyResponseGraph::insertVertex(int vertex) {
     adjList[vertex] = {};
 }
 
+// Adds the destination vertex and distance to the source vertex's list
+// Adds a new vertex to the graph if it does not exist
 void EmergencyResponseGraph::insertEdge(int source, int dest, float dist) {
     adjList[source].push_back(make_pair(dest, dist));
     adjList[dest].push_back(make_pair(source, dist));
 }
 
+// Stores the coordinates for a given vertex
 void EmergencyResponseGraph::insertCoordinates(int vertex, float lat, float lon) {
     if (coordinates.find(vertex) == coordinates.end()) {
         coordinates[vertex] = make_pair(lat, lon);
     }
 }
 
+// Returns all vertices adjacent to the given vertex
 vector<int> EmergencyResponseGraph::getAdjacent(int vertex) const{
     vector<int> adjacent;
     for (auto& p : adjList.at(vertex)) {
@@ -25,6 +29,7 @@ vector<int> EmergencyResponseGraph::getAdjacent(int vertex) const{
     return adjacent;
 }
 
+// Returns the distance between two specified vertices and 0 is no edge exists between them
 float EmergencyResponseGraph::getDistance(int source, int dest) const {
     for (auto& adjacent : adjList.at(source)) {
         if (adjacent.first == dest) {
@@ -34,6 +39,7 @@ float EmergencyResponseGraph::getDistance(int source, int dest) const {
     return 0;
 }
 
+// Returns a vector of all vertices in the graph
 vector<int> EmergencyResponseGraph::getVertices() const {
     vector<int> vertices;
     vertices.reserve(adjList.size());
@@ -43,7 +49,7 @@ vector<int> EmergencyResponseGraph::getVertices() const {
     return vertices;
 }
 
-
+// Returns a vector of all edges in the graph
 vector<tuple<int, int, float>> EmergencyResponseGraph::getEdges() const {
     vector<tuple< int, int, float>> edges;
     for (auto& kv : adjList) {
@@ -57,6 +63,7 @@ vector<tuple<int, int, float>> EmergencyResponseGraph::getEdges() const {
     return edges;
 }
 
+// Returns a pair containing the coordinates of a given vertex
 pair<float, float> EmergencyResponseGraph::getCoordinates(int vertex) {
     if (coordinates.find(vertex) == coordinates.end()) {
         return make_pair(0, 0);
@@ -64,6 +71,8 @@ pair<float, float> EmergencyResponseGraph::getCoordinates(int vertex) {
     return coordinates[vertex];
 }
 
+// Returns the ID of the vertex that the "Hospital" is located at
+// This is used as the source vertex for all paths found
 int EmergencyResponseGraph::getHospitalVertex() {
     return hospital_vertex;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 using namespace std::chrono;
 
+// Writes the coordinates of the path to a specified csv file
 void write_path_to_csv(EmergencyResponseGraph &graph, vector<int> path, string filename) {
    string file_path = "../" + filename;
    ofstream outputFile(file_path, ios::out);
@@ -27,15 +28,14 @@ int main() {
     // Open the file containing graph data
     fin.open("../NewYorkGraphData.csv", ios::in);
 
+    // Iterate through each row of the csv file
     string line, word;
     while (getline(fin, line)) {
         vector<string> row;
 
-        // used for breaking words
         stringstream s(line);
 
-        // read every column data of a row and
-        // store it in a string variable, 'word'
+        // Reads each column from the current row
         while (getline(s, word, ',')) {
             // add all the column data
             // of a row to a vector
@@ -49,11 +49,13 @@ int main() {
         string dest_lat = row[5];
         string dest_long = row[6];
 
+        // Insert the edge into the graph along with coordinates of each vertex
         graph.insertEdge(stoi(source), stoi(destination), stof(distance));
         graph.insertCoordinates(stoi(source), stof(source_lat), stof(source_long));
         graph.insertCoordinates(stoi(destination), stof(dest_lat), stof(dest_long));
     }
 
+    // Creates dummy vertices that are not connected to any other vertices
     for (int i = 0; i < 148619; i++) {
         if (graph.getCoordinates(i).first == 0) {
             graph.insertVertex(i);
@@ -65,6 +67,8 @@ int main() {
     int dest = -1;
     cout << "An Emergency Has Appeared!!!" << endl;
     cout << endl;
+
+    // Loops through the user menu until a valid option os selected
     while (dest == -1) {
         int select;
         cout << "Please Select An Option:" << endl;
@@ -73,14 +77,18 @@ int main() {
         cout << "Choice:";
         cin >> select;
 
-
+        // Allows the user to choose a destination vertex
         if (select == 1) {
             cout << "Choose a Number Between 0 and " << (graph.getVertices().size() - 1) << endl;
             cout << "Your Desired Destination: ";
             cin >> dest;
-        } else if (select == 2) {
+        }
+
+        // Chooses a random vertex from the graph as the destination
+        else if (select == 2) {
             dest = rand() % (graph.getVertices().size() - 1);
-        } else {
+        }
+        else {
             cout << "Whoops! Not a Valid Option" << endl;
             continue;
         }
@@ -88,6 +96,7 @@ int main() {
 
     cout << endl;
 
+    // Runs and times Dijkstra's Algorithm
     Djikstras d;
     auto start = high_resolution_clock::now();
     vector<int> Dijkstra = d.shortestPath(graph, source, dest);
@@ -95,6 +104,7 @@ int main() {
     auto duration = duration_cast<milliseconds>(end - start);
     cout << "Time Taken For Dijkstra's Algorithm: " << duration.count() << " milliseconds" << endl;
 
+    // Runs and times Bellman-Ford Algorithm
     start = high_resolution_clock::now();
     vector<int> Bellman_Ford = shortestBellmanFordPath(graph, source, dest);
     end = high_resolution_clock::now();
@@ -103,6 +113,7 @@ int main() {
 
     cout << endl;
 
+    // If a path exists, it is written to a csv file
     if (Dijkstra.empty()) {
         cout << "Sorry! No Path Was Found" << endl;
     }
